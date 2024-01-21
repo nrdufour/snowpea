80e59e6e567cdbae5cf7cceec02ae4cc1bc11333
Nicolas Dufour <nrdufour@gmail.com>
2024-01-21 12:07:02 -0500

    feat: Adding support for unstable packages line

--- generic/flake.nix
+++ generic/flake.nix
@@ -3,20 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: rec {
-    nixosConfigurations = {
-      pi4 = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
+    let
+    system = "aarch64-linux";
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
+    in {
+      nixosConfigurations.pi4 = nixpkgs.lib.nixosSystem {
+        inherit system;
         modules = [
+          # Overlays-module makes "pkgs.unstable" available in configuration.nix
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
           ../base
         ];
       };
     };
-
-  };
 }
