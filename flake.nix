a327050d46f78bc24ac0fe47498f4603f6fde72c
Nicolas Dufour <nrdufour@gmail.com>
2024-01-25 19:14:24 -0500

    feat: refactor to combine into one flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Flake for building a Raspberry Pi SD images";
+  description = "Flake for building Raspberry Pi based SD images and machines";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
@@ -16,14 +16,26 @@
       };
     };
     in {
-      nixosConfigurations.pi4 = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.genpi4 = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           # Overlays-module makes "pkgs.unstable" available in configuration.nix
           ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-          ../../nixos/base
-          ({ sdImage.imageName = "pi4.img"; })
+          ./nixos/base
+          ({ sdImage.imageName = "genpi4.img"; })
+        ];
+      };
+
+      nixosConfigurations.eagle = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          # Overlays-module makes "pkgs.unstable" available in configuration.nix
+          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
+          ./nixos/base
+          ./nixos/apps/gitea.nix
+          ({ sdImage.imageName = "eagle.img"; })
         ];
       };
     };
