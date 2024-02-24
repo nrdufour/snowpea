a124488829323f0012b5493a09c624982d55be07
Nicolas Dufour <nrdufour@gmail.com>
2024-02-24 15:14:29 -0500

    feat(sops): adding sops-nix to the fold

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, sops-nix }@inputs:
     let
     system = "aarch64-linux";
     overlay-unstable = final: prev: {
@@ -47,6 +48,7 @@
           "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
           ./nixos/hosts/myca
           ({ sdImage.imageName = "myca.img"; })
+          sops-nix.nixosModules.sops
         ];
       };
     };
