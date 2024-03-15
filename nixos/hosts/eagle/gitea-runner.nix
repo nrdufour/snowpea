b6fbc13951200352480df2f34ada5c28f97c2815
Nicolas Dufour <nrdufour@gmail.com>
2024-03-15 10:35:56 -0400

    fix(gitea-runner): fixing the previous issue with shell and action

--- nixos/hosts/eagle/gitea-runner.nix
+++ nixos/hosts/eagle/gitea-runner.nix
@@ -1,18 +1,45 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   sops.secrets = {
     gitea_runner_token = {};
   };
 
+  #
+  # Ref: https://github.com/colonelpanic8/dotfiles/blob/03346eeaeb68633a50d6687659cbcdf46d243d36/nixos/gitea-runner.nix#L20
+  # 
+
   services.gitea-actions-runner = {
     instances = {
-      eagle = {
+      eagle = let gitea-runner-directory = "/var/lib/gitea-runner"; in {
+        settings = {
+          cache = {
+            enabled = true;
+          };
+          container = {
+            workdir_parent = "${gitea-runner-directory}/workspace";
+          };
+          host = {
+            workdir_parent = "${gitea-runner-directory}/action-cache-dir";
+          };
+        };
         enable = true;
         name = "eagle";
         url = "https://git.internal/";
         tokenFile = config.sops.secrets.gitea_runner_token.path;
         labels = [
           "native:host"
+        ];
+        hostPackages = with pkgs; [
+          bash
+          coreutils
+          curl
+          gawk
+          git-lfs
+          nixFlakes
+          gitFull
+          gnused
+          nodejs
+          wget
         ];
       };
     };
