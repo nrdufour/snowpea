4e93a1780b0aa83510043f66db6f551526ebd24e
Nicolas Dufour <nrdufour@gmail.com>
2024-06-07 20:10:06 -0400

    feat: adding files from the rk3588 project

---
+++ nixos/modules/rk3588/boards/orangepi5plus.nix
@@ -0,0 +1,50 @@
+# =========================================================================
+#      Orange Pi 5 Plus Specific Configuration
+# =========================================================================
+{
+  pkgs,
+  rk3588,
+  ...
+}: let
+  pkgsKernel = rk3588.pkgsKernel;
+in {
+  imports = [
+    ./base.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgsKernel.linuxPackagesFor (pkgsKernel.callPackage ../../pkgs/rk35xx-kernel/vendor.nix {});
+
+    # kernelParams copy from Armbian's /boot/armbianEnv.txt & /boot/boot.cmd
+    kernelParams = [
+      "rootwait"
+
+      "earlycon" # enable early console, so we can see the boot messages via serial port / HDMI
+      "consoleblank=0" # disable console blanking(screen saver)
+      "console=ttyS2,1500000" # serial port
+      "console=tty1" # HDMI
+
+      # docker optimizations
+      "cgroup_enable=cpuset"
+      "cgroup_memory=1"
+      "cgroup_enable=memory"
+      "swapaccount=1"
+    ];
+  };
+
+  # add some missing deviceTree in armbian/linux-rockchip:
+  # orange pi 5 plus's deviceTree in armbian/linux-rockchip:
+  #    https://github.com/armbian/linux-rockchip/blob/rk-5.10-rkr4/arch/arm64/boot/dts/rockchip/rk3588-orangepi-5-plus.dts
+  hardware = {
+    deviceTree = {
+      # https://github.com/armbian/build/blob/f9d7117/config/boards/orangepi5-plus.wip#L10C51-L10C51
+      name = "rockchip/rk3588-orangepi-5-plus.dtb";
+      overlays = [
+      ];
+    };
+
+    firmware = [
+      (pkgs.callPackage ../../pkgs/orangepi-firmware {})
+    ];
+  };
+}
