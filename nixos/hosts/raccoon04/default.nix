f4eaee0072d69df4c717e965bdfc533eb4d6e7b8
Nicolas Dufour <nrdufour@gmail.com>
2025-02-27 08:02:50 -0500

    feat: forcing the use of usb-storage over uas on raspberry pi 4 using the usb m2 extension card as uas is not fully supported

--- nixos/hosts/raccoon04/default.nix
+++ nixos/hosts/raccoon04/default.nix
@@ -26,4 +26,13 @@
     "--node-label svccontroller.k3s.cattle.io/enablelb=true"
     "--node-label svccontroller.k3s.cattle.io/lbpool=internal"
   ];
+
+  # Avoid using UAS driver for this usb device, as it's not stable with the raspberry pi 4
+  # See https://www.reddit.com/r/NixOS/comments/okpz7w/rpi4_nixos_quirks_for_ssd_with_usb3_uasp_problem/
+  # and https://forums.raspberrypi.com/viewtopic.php?f=28&t=245931&sid=d8df335a2e30668e9852048027efba3b
+  # and https://linux-sunxi.org/USB/UAS
+  boot.kernelParams = [
+    "usb-storage.quirks=174c:2362:u"
+    "usbcore.quirks=174c:2362:u"
+  ];
 }
