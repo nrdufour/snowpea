64699a0335fe0dae2b5661e4814f0b708719d31c
Nicolas Dufour <nrdufour@gmail.com>
2025-06-26 08:48:45 -0400

    feat(routy): missing forward and adding some vlans

--- nixos/hosts/routy/network.nix
+++ nixos/hosts/routy/network.nix
@@ -1,5 +1,22 @@
 { pkgs, config, ... }: {
 
+  # Let's make sure we can forward traffic between the host and the router.
+  boot = {
+    kernel = {
+      sysctl = {
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
+        # TODO: Configure IPV6
+        "net.ipv6.conf.wan.disable_ipv6" = true;
+        # "net.ipv6.conf.all.accept_ra" = 0;
+        # "net.ipv6.conf.all.autoconf" = 0;
+        # "net.ipv6.conf.all.use_tempaddr" = 0;
+        # "net.ipv6.conf.wan.accept_ra" = 2;
+        # "net.ipv6.conf.wan.autoconf" = 1;
+      };
+    };
+  };
+
   networking = {
     hostName = "routy";
     domain   = "internal";
@@ -40,25 +57,33 @@
       };
     };
 
-    # netdevs = {
-    #   # VLANs
-    #   "20-lan0.20" = {
-    #     netdevConfig = {
-    #       Name = "lan0.20";
-    #       Description = "HOME";
-    #       Kind = "vlan";
-    #     };
-    #     vlanConfig.Id = 20;
-    #   };
-    #   "20-lan0.50" = {
-    #     netdevConfig = {
-    #       Name = "lan0.50";
-    #       Description = "GUEST";
-    #       Kind = "vlan";
-    #     };
-    #     vlanConfig.Id = 50;
-    #   };
-    # };
+    netdevs = {
+      # VLANs
+      "20-lan0.20" = {
+        netdevConfig = {
+          Name = "lan0.20";
+          Description = "HOME";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 20;
+      };
+      "20-lan0.50" = {
+        netdevConfig = {
+          Name = "lan0.50";
+          Description = "GUEST";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 50;
+      };
+      "20-lan0.100" = {
+        netdevConfig = {
+          Name = "lan0.100";
+          Description = "IOT";
+          Kind = "vlan";
+        };
+        vlanConfig.Id = 100;
+      };
+    };
 
     networks = {
       "30-wan0" = {
@@ -74,6 +99,11 @@
         matchConfig.Name = "lan0";
         address = [ "10.0.0.1/24" ];
         linkConfig.RequiredForOnline = "carrier";
+        vlan = [
+          "lan0.20" # HOME
+          "lan0.50" # GUEST
+          "lan0.100" # GUEST
+        ];
       };
 
       "30-lab0" = {
@@ -84,8 +114,29 @@
 
       "30-lab1" = {
         matchConfig.Name = "lab1";
-        address = [ "10.1.1.1/24" ];
+        address = [ "10.2.0.1/24" ];
         linkConfig.RequiredForOnline = "carrier";
+      };
+
+      # HOME VLAN
+      "30-lan0.20" = {
+        matchConfig.Name = "lan0.20";
+        address = [ "10.0.20.1/24" ];
+        linkConfig.RequiredForOnline = "routable";
+      };
+
+      # GUEST VLAN
+      "30-lan0.50" = {
+        matchConfig.Name = "lan0.50";
+        address = [ "10.0.50.1/24" ];
+        linkConfig.RequiredForOnline = "routable";
+      };
+
+      # IOT VLAN
+      "30-lan0.100" = {
+        matchConfig.Name = "lan0.100";
+        address = [ "10.0.100.1/24" ];
+        linkConfig.RequiredForOnline = "routable";
       };
     };
   };
