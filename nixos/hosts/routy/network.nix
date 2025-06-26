8f04486ac3bec69184e9c390a8b906187a3bcffa
Nicolas Dufour <nrdufour@gmail.com>
2025-06-26 11:03:03 -0400

    feat(routy): understand better how systemd-networkd works and using the local knot dns in the resolv

--- nixos/hosts/routy/network.nix
+++ nixos/hosts/routy/network.nix
@@ -23,6 +23,7 @@
     useDHCP  = false;
 
     nameservers = [
+      "10.0.0.1"
       "1.1.1.1"
       "8.8.8.8"
     ];
@@ -33,9 +34,10 @@
   systemd.network = {
     enable = true;
 
-    # Avoid blocking, even though we don't have NetworkManager enabled
-    # in the same time
-    wait-online.enable = false;
+    # Enabling it after all, as long as we understand the fields `RequiredForOnline`
+    # and `ActivationPolicy` used in the linkConfig below
+    ## Ref: https://www.man7.org/linux/man-pages/man5/systemd.network.5.html
+    wait-online.enable = true;
 
     links = {
       # rename all interface names to be easier to identify
@@ -98,7 +100,10 @@
       "30-lan0" = {
         matchConfig.Name = "lan0";
         address = [ "10.0.0.1/24" ];
-        linkConfig.RequiredForOnline = "carrier";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
         vlan = [
           "lan0.20" # HOME
           "lan0.50" # GUEST
@@ -109,34 +114,49 @@
       "30-lab0" = {
         matchConfig.Name = "lab0";
         address = [ "10.1.0.1/24" ];
-        linkConfig.RequiredForOnline = "carrier";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
       };
 
       "30-lab1" = {
         matchConfig.Name = "lab1";
         address = [ "10.2.0.1/24" ];
-        linkConfig.RequiredForOnline = "carrier";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
       };
 
       # HOME VLAN
       "30-lan0.20" = {
         matchConfig.Name = "lan0.20";
         address = [ "10.0.20.1/24" ];
-        linkConfig.RequiredForOnline = "routable";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
       };
 
       # GUEST VLAN
       "30-lan0.50" = {
         matchConfig.Name = "lan0.50";
         address = [ "10.0.50.1/24" ];
-        linkConfig.RequiredForOnline = "routable";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
       };
 
       # IOT VLAN
       "30-lan0.100" = {
         matchConfig.Name = "lan0.100";
         address = [ "10.0.100.1/24" ];
-        linkConfig.RequiredForOnline = "routable";
+        linkConfig = {
+          ActivationPolicy = "always-up";
+          RequiredForOnline = "no";
+        };
       };
     };
   };
