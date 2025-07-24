716f71ecfbc32ccc59de19e36dad4f68063784b4
Nicolas Dufour <nrdufour@gmail.com>
2025-07-24 09:51:51 -0400

    feat(monitoring): enable smartctl exporter

--- nixos/modules/nixos/services/monitoring.nix
+++ nixos/modules/nixos/services/monitoring.nix
@@ -29,8 +29,21 @@ in
           ];
           port = exporterPort;
         };
+        smartctl = {
+          enable = true;
+        };
       };
     };
+
+    systemd.services."prometheus-smartctl-exporter".serviceConfig.DeviceAllow = lib.mkOverride 10 [
+        "block-blkext rw"
+        "block-sd rw"
+        "char-nvme rw"
+    ];
+
+    services.udev.extraRules = ''
+      SUBSYSTEM=="nvme", KERNEL=="nvme[0-9]*", GROUP="disk"
+    '';
 
     networking.firewall = {
       allowedTCPPorts = [ exporterPort ];
