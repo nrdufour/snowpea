d3311ebc60d6956005f1cf1a5c4542c9251b57e0
Nicolas Dufour <nrdufour@gmail.com>
2025-07-31 10:27:59 -0400

    feat(cardinal): preparing restic for media as well

--- nixos/hosts/cardinal/backups/local/restic-nfs.nix
+++ nixos/hosts/cardinal/backups/local/restic-nfs.nix
@@ -21,11 +21,27 @@
       };
     };
 
+    # media = {
+    #   paths = [
+    #     "/tank/Media"
+    #   ];
+      
+    #   initialize = true;
+    #   repositoryFile  = config.sops.secrets."backups/media/repository".path;
+    #   passwordFile    = config.sops.secrets."backups/local-restic/password".path;
+
+    #   timerConfig = {
+    #     OnCalendar = "*-*-* 10:00:00";
+    #     Persistent = true;
+    #   };
+    # };
+
   };
 
   sops = {
     secrets = {
       "backups/books/repository" = {};
+      "backups/media/repository" = {};
 
       "backups/local-restic/password" = {
         sopsFile = ../../../../../secrets/common-local-restic/secrets.sops.yaml;
